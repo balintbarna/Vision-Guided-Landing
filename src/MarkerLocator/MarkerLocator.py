9f187dc6c181a9ba2b6b4c54a0b0277507883fa6
frnyb <frnyb@student.sdu.dk>
2020-05-06 10:37:55 +0200

    MarkerLocator

--- src/MarkerLocator/MarkerLocator.py
+++ src/MarkerLocator/MarkerLocator.py
@@ -20,9 +20,9 @@ from MarkerTracker import MarkerTracker
 # parameters
 print_debug_messages = False
 show_image = True
-list_of_markers_to_find = [4]
+list_of_markers_to_find = [4, 5]
 get_images_to_flush_cam_buffer = 5
-publish_to_ros = True
+publish_to_ros = False
 markerpose_ros_topic = '/markerlocator/markerpose'
 
 # global variables
--- src/MarkerLocator/setup.py
+++ src/MarkerLocator/setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  scripts=['MarkerLocatorROS.py', 'MarkerCaptureROS.py'],
-  packages=['MarkerLocator'])
+  scripts=['MarkerLocatorROS.py', 'MarkerCaptureROS.py'],)
 setup(**d)
 

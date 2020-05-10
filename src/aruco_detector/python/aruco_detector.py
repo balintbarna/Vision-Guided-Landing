0f906112d6b9f139ab5eceb160de384746d203ad
frnyb <frnyb@student.sdu.dk>
2020-05-10 17:39:28 +0200

    Changes for handin

--- src/aruco_detector/python/aruco_detector.py
+++ src/aruco_detector/python/aruco_detector.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# Not finished
+
 import numpy as np
 import cv2
 import rospy
@@ -60,6 +62,8 @@ class ArUcoDetector():
                     np.zeros(4)
             )
 
+	    # Publish estimated pose
+
 
     
 
@@ -67,40 +71,3 @@ class ArUcoDetector():
 
 if __name__ == '__main__':
     pass
-
-frame = cv2.imread("rmurv2_2020_marker/meshes/miniproject4_marker.png")
-
-
-parameters = cv2.aruco.DetectorParameters_create()
-parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
-dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
-marker_id = 0
-camera_matrix = np.array([
-                [448.1, 0, 376],
-                [0, 448.1, 240],
-                [0, 0, 1]
-            ])
-aruco_length = 0.05
-
-corners, ids, rejected_pts = cv2.aruco.detectMarkers(
-        frame, 
-        dictionary, 
-        parameters=parameters
-)
-
-if marker_id in ids:
-    index = list(ids).index(marker_id)
-    corners = np.array(list(corners[0][index])).astype(np.int32)
-
-    rvecs, tvecs = cv2.aruco.estimatePoseSingleMarkers(
-            corners,
-            aruco_length,
-            camera_matrix,
-            np.array([[0,0,0,0]], dtype=np.float)
-    )
-
-#for i in range(4):
-#    img = cv2.line(img, tuple(corners[i]), tuple(corners[i + 1]), (0,0,255), thickness=6)
-#
-##rect = cv2.rectangle(img, tuple(corners[0][0][0]), tuple(corners[0][0][2]), (255,0,0))
-#cv2.imwrite("img.png", img)
